eee629e5e073d70af9e71f3d7572f530c8755d8b
MustapheMS <moztaphem9@gmail.com>
2023-06-15 21:02:48 +0300

    Second Largest Done 100%

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+
+//Done 💯💯👍👍
+
+function secondLargest(arr) {
+    arr.sort(function(a, b) {
+      return b - a;
+    });
+    return arr[1];
+  }
+  console.log(secondLargest([44, 55, 66, 77, 88])); // Result --> 77
+  console.log(secondLargest([10, 30, 80, 60, 100])); // Result --> 80
+  
